8f2f1526dd38475c9dfc8d8651be23d6b7dbee24
romi9872 <adian281r@gmail.com>
2021-03-20 13:54:37 +0700

    Membuat API Endpoint Login dan Logout

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/auth/register', 'Api/Auth/RegisterController');
+Route::post('/auth/register', 'Api\Auth\RegisterController');
+Route::post('/auth/login', 'Api\Auth\LoginController@login');
+Route::post('/auth/logout', 'Api\Auth\LoginController@logout')->middleware('auth:api');
